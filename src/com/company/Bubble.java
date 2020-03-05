33b2992e8786a766b36cb0260db7c9df84e2e5dc
PURAT <alex-shtunder@yandex.ru>
2020-03-05 19:04:17 +0300

    Bubble sorting

---
+++ src/com/company/Bubble.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Bubble {
+    public static void bubbleSortingAscending(double[] array) {
+        for (int out = array.length - 1; out >= 1; out--) {
+            for (int in = 0; in < out; in++) {
+                if (array[in] > array[out]) {
+                    double tmp = array[in];
+                    array[in] = array[in + 1];
+                    array[in + 1] = tmp;
+                }
+            }
+        }
+    }
+
+
+}
