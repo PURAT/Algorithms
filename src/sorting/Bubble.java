52ff0aed1ccb2f2beb9d7642e2b0a35e85bd80e9
PURAT <alex-shtunder@yandex.ru>
2020-03-05 19:10:41 +0300

    Refactor bubble sorting.

--- src/sorting/Bubble.java
+++ src/sorting/Bubble.java
@@ -1,10 +1,10 @@
-package com.company;
+package sorting;
 
 public class Bubble {
     public static void bubbleSortingAscending(double[] array) {
         for (int out = array.length - 1; out >= 1; out--) {
             for (int in = 0; in < out; in++) {
-                if (array[in] > array[out]) {
+                if (array[in] > array[in + 1]) {
                     double tmp = array[in];
                     array[in] = array[in + 1];
                     array[in + 1] = tmp;
