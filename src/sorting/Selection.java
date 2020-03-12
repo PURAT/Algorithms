4fc1fe7ff7c659d0aeb153ad3eccea886f645a17
PURAT <alex-shtunder@yandex.ru>
2020-03-12 22:30:10 +0300

    Added selection sort.

---
+++ src/sorting/Selection.java
@@ -0,0 +1,17 @@
+package sorting;
+
+public class Selection {
+    public static void selectionSortingAscending(double[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            int min_i = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[min_i]) {
+                    min_i = j;
+                }
+            }
+            double tmp = array[i];
+            array[i] = array[min_i];
+            array[min_i] = tmp;
+        }
+    }
+}
